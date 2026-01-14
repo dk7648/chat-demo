93495b75b7dc96e29f1e0b038b867f72fcde3b93
campus3 S018 <s018@alpacoedu.onmicrosoft.com>
2026-01-14 11:07:39 +0900

    Add application.py

--- application.py
+++ application.py
@@ -8,24 +8,14 @@ from starlette.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()  # Load .env variables
-
-if os.environ.get("ENV_STATE") == "prod":
-    print("Production mode")
-    broadcast = Broadcast("redis://"+os.environ.get('REDIS_HOST')+":6379")
-else:
-    print("Development mode")
-    broadcast = Broadcast("redis://127.0.0.1:6379")
+REDIS_HOST = os.environ.get('REDIS_HOST', 'redis-ro.iy2jdd.ng.0001.apn2.cache.amazonaws.com')
+broadcast = Broadcast(f"redis://{REDIS_HOST}:6379")
 templates = Jinja2Templates("templates")
-
-
 async def homepage(request):
     template = "index.html"
     context = {"request": request}
     return templates.TemplateResponse(template, context)
-
-
 async def chatroom_ws(websocket):
     await websocket.accept()
     channel_name = "demo"
@@ -33,38 +23,31 @@ async def chatroom_ws(websocket):
         (chatroom_ws_receiver, {"websocket": websocket, "channel_name": channel_name}),
         (chatroom_ws_sender, {"websocket": websocket, "channel_name": channel_name}),
     )
-
-
 async def chatroom_ws_receiver(websocket, channel_name):
     async for message in websocket.iter_text():
+        print(f"Received message: {message}")
         await broadcast.publish(channel=channel_name, message=message)
-
-
+        print(f"Published message to channel: {channel_name}")
 async def chatroom_ws_sender(websocket, channel_name):
     async with broadcast.subscribe(channel=channel_name) as subscriber:
         async for event in subscriber:
             await websocket.send_text(
                 event.message
             )
-
-
 routes = [
     Route("/", homepage),
     WebSocketRoute("/", chatroom_ws, name='chatroom_ws'),
 ]
-
 origins = [
     "http://localhost",
     "http://localhost:8000"
 ]
-
 middleware = [
     Middleware(CORSMiddleware,
                allow_origins=origins,
                allow_methods=['*'],
                allow_headers=['*'])
 ]
-
 app = Starlette(
     routes=routes,
     on_startup=[broadcast.connect],
